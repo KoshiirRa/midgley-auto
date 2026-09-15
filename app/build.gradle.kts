79201f390282fc93b624206eb97ac0b90b70d055
KoshiirRa <m.cubed.3@gmail.com>
2026-09-14 19:41:09 -0500

    chore(release): bump version to 1.1.0 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.n2yti.midgley.auto"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
